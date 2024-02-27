8ef60326d930b567e878443545415b0872c6a13d
김현수 <soo@Soos-Mac.local>
2024-02-27 15:17:14 +0900

    init: 실습 프로젝트 생성
    프로젝트 생성

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String name = "홍길동"; // 홍길동
+        System.out.println(name + " 20세");
+    }
+}
