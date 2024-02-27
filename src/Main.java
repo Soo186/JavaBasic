bc457eb7e8d8fe8c5c3181d95a442cb9a920c677
Soo186 <soo@Soos-Mac.local>
2024-02-27 15:39:55 +0900

    안녕하세요 출력

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         String name = "홍길동"; // 홍길동
         System.out.println(name + " 20세");
+        System.out.println("안녕하세요");
     }
 }
